4235baa13107c75084f6066776301dd09dc30b77
MatthewGwapo <122527157+MatthewGwapo@users.noreply.github.com>
2023-03-30 23:46:59 +0800

    Opiano_Calculator

---
+++ app/src/main/java/com/firstapp/opiano_calculator/MainActivity.java
@@ -0,0 +1,19 @@
+private ActivityMainBinding binding;
+
+
+private Double val1 = Double.NaN;
+private Double val2;
+
+private static final char ADDITION = '+';
+private static final char SUBTRACTION = '-';
+private static final char MULTIPLICATION = '*';
+private static final char DIVISION = '/';
+
+private char CURRENT_ACTION;
+
+
+
+private DecimalFormat decimalFormat;
+
+
+
